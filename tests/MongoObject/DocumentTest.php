c7e0db50e5614880901d576b9e0c35dad8df3519
Tikhon Tagunov <niptuckker@gmail.com>
2014-01-09 15:31:53 +0000

    Test for Document class, method create

---
+++ tests/MongoObject/DocumentTest.php
@@ -0,0 +1,41 @@
+<?php
+
+require_once('PHPUnit/Autoload.php');
+require_once('../../documongo/Load.php');
+
+use \documongo\MongoObject\DocumentType;
+use \documongo\MongoObject\Document;
+
+class DocumentTest extends PHPUnit_Framework_TestCase
+{
+    public function testCreate()
+    {
+
+        $mn = new \MongoClient();
+        $prefix = "temp_test_";
+
+        $typeObject = DocumentType::findByType($mn, $prefix, "faculty");
+
+        $uuid = "test-uuid";
+
+        $testDocument = Document::create($mn, $prefix, $typeObject, $uuid);
+
+        $this->assertEquals($testDocument->uuid, $uuid);
+
+        $ok = $testDocument->save();
+
+        $this->assertEquals($ok, true);
+
+
+        $testDocument2 = Document::findByUuid($mn, $prefix, $uuid);
+
+        $this->assertEquals($testDocument2->uuid, $uuid);
+
+        $this->assertEquals($testDocument, $testDocument2);
+
+        $deleted = $testDocument2->delete();
+
+        $this->assertEquals($deleted, true);
+    }
+}
+
